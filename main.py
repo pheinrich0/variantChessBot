588c7b355f7d1715c66cd4f522ec51352f6cf72b
David Weingut <da.we_2003@web.de>
2022-06-09 23:00:00 +0200

    implement first version of negamax

--- main.py
+++ main.py
@@ -1,8 +1,86 @@
 import chess
-import sys
-sys.path.append("../lichess-bot/")
-import strategies
+import chess.variant
+from chess.engine import PlayResult, InfoDict, PovScore, Cp
+import time
 
-class VariantBot(MinimalEngine):
-   def search(self, board, time_limit, ponder, draw_offered):
-      return PlayResult(random.choice(list(board.legal_moves)), None)
+mateScore = 10000
+
+
+def negamax(board, depth, ply):
+
+    if board.is_game_over():
+        result = board.outcome()
+        us = board.turn
+        return (result.winner is us) * mateScore - (result.winner is not us) * mateScore
+    elif depth == 0:
+        return evaluate(board)
+
+    bestScore = -100000
+    bestMove = chess.Move(chess.A2, chess.A4)
+    for m in board.legal_moves:
+        board.push(m)
+        tempScore = -negamax(board, depth - 1, ply + 1)[0]
+        board.pop()
+        if tempScore > bestScore:
+            bestScore = tempScore
+            bestMove = m
+    return bestScore, bestMove
+
+
+matScores = [100, 315, 330, 500, 900, 200]
+
+
+def evaluate(board, depth):
+    result = board.outcome()
+    if result:
+        us = board.turn
+        return (result.winner is us) * mateScore - (result.winner is not us) * mateScore
+
+    bType = type(board).uci_variant
+    if bType == chess.variant.AntichessBoard:
+        return evaluateAnti(board, depth)
+    else:
+        return evaluateStandard(board, depth)
+
+
+def evaluateStandard(board, depth):
+    eval = 0
+    for pt in range(1, 7):
+        eval += len([sq for sq in board.pieces(pt, 1)]) * matScores[pt - 1]
+        eval -= [sq for sq in board.pieces(pt, 0)] * matScores[pt - 1]
+    if board.turn:
+        return eval
+    else:
+        return -eval
+
+
+def evaluateAnti(board, depth):
+    return -evaluateStandard(board, depth)
+
+
+def iterativeDeepening(board, time_limit):
+    us = board.turn
+    avtime = 0
+    if us:
+        avtime += time_limit.white_clock / 100
+        inc = time_limit.white_inc
+        if inc * 3 < avtime:
+            avtime += (inc * 7) / 15
+    bestMove = board.legal_moves[0]
+    score = 0
+    starttime = time.time()
+    depth = 1
+    while time.time() - starttime < avtime:
+        depth += 1
+        score, bestMove = negamax(board, depth, 1)
+        print(
+            f"info depth {depth} score cp {score} time {round((time.time()-starttime)*1000)} pv {bestMove.uci()}\n"
+        )
+    print(f"bestmove {bestMove.uci()}\n")
+    info = InfoDict(
+        score=PovScore(Cp(score), us),
+        pv=[bestMove],
+        depth=depth,
+        time=time.time() - starttime,
+    )
+    return PlayResult(bestMove, info=info)
