0e0f262774bae290d7bffe25abcb0574851fb05c
David Weingut <da.we_2003@web.de>
2022-06-11 16:21:05 +0200

    add easy entrypoint to customize eval functions for variants

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import chess
 import chess.variant
 from chess.engine import PlayResult, InfoDict, PovScore, Cp
-
+import sys
 import time
 
 mateScore = 10000
 
 
-def negamax(board, depth, ply, alpha=-10000000, beta=10000000):
-    if board.is_game_over():
-        result = board.outcome()
-        us = board.turn
-        return (result.winner is us) * mateScore - (result.winner is not us) * mateScore, 0
-    elif depth == 0:
-        return -evaluate(board, depth), 0
+def negamax(board, depth, ply, alpha=-sys.maxsize, beta=sys.maxsize):
+    if depth == 0 or board.is_game_over(claim_draw=board.ply() > 70):
+        return evaluate(board, depth), 0
 
-    bestScore = -100000
+    bestScore = -sys.maxsize
     bestMove = chess.Move(chess.A2, chess.A4)
     for m in board.legal_moves:
         board.push(m)
@@ -24,8 +20,8 @@ def negamax(board, depth, ply, alpha=-10000000, beta=10000000):
         if tempScore > bestScore:
             bestScore = tempScore
             bestMove = m
-            if bestScore > alpha:
-                alpha = bestScore
+        if bestScore > alpha:
+            alpha = bestScore
         if alpha >= beta:
             break
     return bestScore, bestMove
@@ -35,36 +31,82 @@ matScores = [100, 315, 330, 500, 900, 200]
 
 
 def evaluate(board, depth):
-    result = board.outcome()
+    result = board.outcome(claim_draw=board.ply() > 70)
     if result:
         us = board.turn
         return (result.winner is us) * mateScore * (depth + 1) - (
             result.winner is not us
         ) * mateScore * (depth + 1)
-
-    bType = type(board).uci_variant
+    bType = type(board)
     if bType == chess.variant.AntichessBoard:
         return evaluateAnti(board, depth)
+    elif bType == chess.variant.AtomicBoard:
+        return evaluateAtomic(board, depth)
+    elif bType == chess.variant.KingOfTheHillBoard:
+        return evaluateKOTH(board, depth)
+    elif bType == chess.variant.RacingKingsBoard:
+        return evaluateRacing(board, depth)
+    elif bType == chess.variant.HordeBoard:
+        return evaluateHorde(board, depth)
+    elif bType == chess.variant.ThreeCheckBoard:
+        return evaluate3check(board, depth)
+    elif bType == chess.variant.CrazyhouseBoard:
+        return evaluateStandard(board, depth)
     else:
         return evaluateStandard(board, depth)
 
 
 def evaluateStandard(board, depth):
     eval = 0
+    us = board.turn
+    opp = not us
     for pt in range(1, 7):
-        eval += len(board.pieces(pt, 1)) * matScores[pt - 1]
-        eval -= len(board.pieces(pt, 0)) * matScores[pt - 1]
-    if board.turn:
-        return eval
-    else:
-        return -eval
+        eval += len(board.pieces(pt, us)) * matScores[pt - 1]
+        eval -= len(board.pieces(pt, opp)) * matScores[pt - 1]
+    return eval
 
 
 def evaluateAnti(board, depth):
-    return -evaluateStandard(board, depth)
+    eval = -evaluateStandard(board, depth)
+    return eval
 
 
-def iterativeDeepening(board, time_limit):
+def evaluateAtomic(board, depth):
+    eval = evaluateStandard(board, depth)
+    return eval
+
+
+def evaluateKOTH(board, depth):
+    eval = evaluateStandard(board, depth)
+    return eval
+
+
+def evaluateRacing(board, depth):
+    eval = evaluateStandard(board, depth)
+    return eval
+
+
+def evaluateHorde(board, depth):
+    eval = evaluateStandard(board, depth)
+    return eval
+
+
+def evaluate3check(board, depth):
+    eval = evaluateStandard(board, depth)
+    return eval
+
+
+def evaluateCrazyhouse(board, depth):
+    eval = 0
+    us = board.turn
+    opp = not us
+    pockets = board.pockets
+    for pt in range(1, 6):
+        eval += (pockets[us].count(pt) * matScores[pt - 1]) // 3
+        eval -= (pockets[opp] * matScores[pt - 1]) // 3
+
+
+def iterativeDeepening(board, time_limit, uciLogging=False):
     us = board.turn
     avtime = 0
     print(time_limit)
@@ -89,10 +131,12 @@ def iterativeDeepening(board, time_limit):
     while (time.time() - starttime < avtime) and depth < 100:
         depth += 1
         score, bestMove = negamax(board, depth, 1)
-        print(
-            f"info depth {depth} score cp {score} time {round((time.time()-starttime)*1000)} pv {bestMove.uci()}\n"
-        )
-    print(f"bestmove {bestMove.uci()}\n")
+        if uciLogging:
+            print(
+                f"info depth {depth} score cp {score} time {round((time.time()-starttime)*1000)} pv {bestMove.uci()}"
+            )
+    if uciLogging:
+        print(f"bestmove {bestMove.uci()}")
     info = InfoDict(
         score=PovScore(Cp(score), us),
         pv=[bestMove],
