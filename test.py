461a1c56e0f01365021903b7fbf11af6b66402a7
David Weingut <da.we_2003@web.de>
2022-06-11 16:21:16 +0200

    add test file to check if eval errors

---
+++ test.py
@@ -0,0 +1,38 @@
+import main
+import chess
+import chess.variant
+import random
+from collections import namedtuple
+
+timelim = namedtuple("time_limit", "time white_clock white_inc black_clock black_inc")
+t1 = timelim(10.0, None, None, None, None)
+t2 = timelim(None, 60.0, 5.0, 60.0, 5.0)
+# b = chess.Board()
+# main.iterativeDeepening(b, t)
+# main.iterativeDeepening(b, t2)
+
+for str in [
+    "standard",
+    "antichess",
+    "atomic",
+    "kingofthehill",
+    "racingkings",
+    "horde",
+    "3check",
+    "crazyhouse",
+]:
+    btype = chess.variant.find_variant(str)
+    b = btype()
+    for i in range(1, 30):
+        m = random.choice(list(b.legal_moves))
+        b.push(m)
+    if b.is_game_over():
+        print(f"aborted test of {str}, game over after test moves\n")
+        continue
+    if str == "crazyhouse":
+        print(b.pockets)
+    print(f"now testing {str}\n")
+
+    main.iterativeDeepening(b, t1)
+    main.iterativeDeepening(b, t2)
+    print()
